d25679b3411c1f26f2ca9a6869d70ddbffbce75a
Jhon coneo <jconeo117@gmail.com>
2023-02-20 00:35:25 -0500

    fix

--- app/head.js
+++ app/head.js
@@ -8,16 +8,6 @@ export default function Head () {
         async src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5023080932601600'
         crossorigin='anonymous'
       />
-     <!-- Google tag (gtag.js) - Google Analytics -->
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-Z4BD0W38FS">
-      </script>
-      <script>    
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments)}
-        gtag('js', new Date());
-
-        gtag('config', 'G-Z4BD0W38FS');
-      </script>
       <link rel='icon' href='/Monkey_records.ico' />
       <link rel='preload' href='../styles/global.css' as='style' onload="this.onload=null;this.rel='stylesheet'" />
     </>
